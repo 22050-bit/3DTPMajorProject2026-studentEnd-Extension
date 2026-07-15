fb15bdbf6074734aa1fffae20256e8cf2675fe60
Allen <ellenfeng6@gmail.com>
2026-07-15 17:38:42 +1200

    pink slip student / application home page is basically done, all buttons and stuff

--- popup.js
+++ popup.js
@@ -1,12 +1,48 @@
-//model code from the first page to reuse in the future
-fetch("https://script.google.com/macros/s/AKfycbxwPo-LNfj3aHkThOpFzQS7pRq_y9H69Ucp-1Chn553sGaybghbViVgkuCMrwmxYJb_qQ/exec")
-    .then(gettingResponse);
-let theResponse = "";
+
 const sheetInput = document.getElementById("testingInput");
 const sheetData = document.getElementById ("testingButton");
 const sheetSend = document.getElementById("testingOutput");
 const symptoms= document.getElementById("wahhh");
 const openOther= document.getElementById("openTheOther");
+
+//code for misc styling of home application page starts here
+
+//reads the teachers there are and files them into the options of the dropdown box
+fetch("teacherNames.json")
+    .then(teacherInfo => teacherInfo.json())
+        .then(teachers => {
+            
+            const dropdownBox = document.getElementById("enterTeacher");
+
+            teachers.forEach(teacher=> {
+                const option = document.createElement("option");
+                option.value = teacher[1];
+                option.textContent = teacher[0];
+                dropdownBox.appendChild(option);
+            }
+
+            );
+ });
+
+//line3 
+const enterTimeValue = document.getElementById("enterTimeValue");
+const enterTime = document.getElementById("enterTime");
+
+function enterTimeChange() {
+    var time=enterTime.value;
+    enterTimeValue.innerHTML = `MAX 60 min, MIN 1 min:<br> ${time} minutes`;
+}
+
+enterTime.addEventListener("input", enterTimeChange);
+
+
+
+//ends here
+
+//model code from the first page to reuse in the future
+fetch("https://script.google.com/macros/s/AKfycbxwPo-LNfj3aHkThOpFzQS7pRq_y9H69Ucp-1Chn553sGaybghbViVgkuCMrwmxYJb_qQ/exec")
+    .then(gettingResponse);
+let theResponse = "";
 
 function Open(){
     window.location.href = "newHome.html";
@@ -56,24 +92,8 @@ sheetData.onclick = test;
 //ends here
 //model code reusable ends here
 
-//code for misc styling of home application page starts here
 
-//reads the teachers there are and files them into the options of the dropdown box
-fetch("teacherNames.json")
-    .then(teacherInfo => teacherInfo.json())
-        .then(teachers => {
-            console.log (teachers);
-            const dropdownBox = document.getElementById("enterTeacher");
 
-            teachers.foreach(teacher=> {
-                const option = document.createElement("option");
-                option.value = "testing";
-                option.textContent = "testing";
-                dropdownBox.appendChild(option);
-            }
 
-            );
-        });
 
-//code for misc styling of home application page starts here
 
