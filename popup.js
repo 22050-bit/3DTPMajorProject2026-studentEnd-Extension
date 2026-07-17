eb798818a250fb51aa6d2e746bb0cdb4206c3b58
Allen <ellenfeng6@gmail.com>
2026-07-17 13:46:30 +1200

    the javascript code to collect information from home page is (mostly) done

--- popup.js
+++ popup.js
@@ -1,3 +1,145 @@
+//LIBRARY OF CALLS TO GET THE ELEMENTS ON HTML TO JS
+
+//HOMEPAGE CONSTS STARTS HERE
+//LINE 1 select teacher
+const enterTeacher = document.getElementById("enterTeacher"); //the enter teacher dropdown box, get value from here
+const saveTeacher = document.getElementById("saveTeacher"); //the green save button for teacher optin
+const cancelTeacher = document.getElementById("cancelTeacher"); //the red "X" button to reset the option
+
+//LINE 2 enter student
+const enterStudent = document.getElementById("enterStudent"); //the enter student name textbox, get value from here
+const saveStudent = document.getElementById("saveStudent"); //the green save button for student  optin
+const cancelStudent = document.getElementById("cancelStudent"); //the red "X" button to reset the option
+
+//LINE 3 time and date
+const enterTimeValue = document.getElementById("enterTimeValue"); //the paragraph to show value
+const enterTime = document.getElementById("enterTime"); //the sliding input to assign time
+
+const enterDate = document.getElementById("enterDate"); //the calander interface, get value here
+
+//line 4 period number
+const enterPeriod = document.getElementsByTagName("enterPeriod"); 
+
+//line 5: reason
+const enterReason = document.getElementById("enterReason");
+const saveReason = document.getElementById("saveReason"); //the green save button for student  optin
+const cancelReason = document.getElementById("cancelReason"); //the red "X" button to reset the option
+
+//line 6 submission
+const submission =document.getElementById("submission"); //submitting button
+
+//STORAGE:
+var studentName;
+var teacher;
+var teacherEmail;
+var timeNeeded;
+var dateOfApplication;
+var periodOfLeave;
+var reasonOfLeave;
+
+//HOMEPAGE CONSTS ENDS HERE
+
+
+//LIBRARY OF CALLS TO GET THE ELEMENTS ON HTML TO JS ENDS HERE
+
+
+//HOME PAGE JS STARTS HERE
+//LINE 1:
+//save the teacher
+function SaveTeacher(){
+    enterTeacher.disabled=true; //no more changing choice
+    saveTeacher.disabled=true; //no more saving, this is already saved
+    teacherEmail=enterTeacher.value; //set value 
+    teacher=enterTeacher.options[enterTeacher.selectedIndex].text;  //set value
+    console.log(teacher + " and "+teacherEmail) //debug testing
+    saveTeacher.style.backgroundColor= "grey";
+}
+saveTeacher.onclick=SaveTeacher;
+
+//cancel the input
+function CancelTeacher(){
+    enterTeacher.disabled=false; //no more changing choice
+    saveTeacher.disabled=false; //no more saving, this is already saved
+    teacherEmail=""; //wipe clean
+    teacher=""; //wipe clean
+    console.log(teacher + " and "+teacherEmail) //debug testing
+    saveTeacher.style.backgroundColor= "green";
+}
+cancelTeacher.onclick=CancelTeacher;
+
+//LINE 2:
+//save the student names
+function SaveStudent(){
+    enterStudent.disabled=true; //no more changing choice
+    saveStudent.disabled=true; //no more saving, this is already saved
+    studentName=enterStudent.value; //set value 
+    console.log(studentName) //debug testing
+    saveStudent.style.backgroundColor= "grey";
+}
+saveStudent.onclick=SaveStudent;
+
+//cancel the input
+function CancelStudent(){
+    enterStudent.disabled=false; //no more changing choice
+    saveStudent.disabled=false; //no more saving, this is already saved
+    studentName=""; //wipe clean
+    console.log(studentName) //debug testing
+    saveStudent.style.backgroundColor= "green";
+}
+cancelStudent.onclick=CancelStudent;
+
+//LINE 3: date and time left
+//for the value shown to correspond with the slider
+function enterTimeChange() {
+    var time=enterTime.value;
+    enterTimeValue.innerHTML = `MAX 60 min, MIN 1 min:<br> ${time} minutes`;
+}
+enterTime.addEventListener("input", enterTimeChange);
+//FLEXIBLE ENTERING; WONT BE SET DEAD HERE
+//DATES; FLEXIBLE ENTERING, WONT BE SET DEAD HERE
+
+//Line 4: period number
+//const periodOfLeave = document.querySelector('input[name="enterPeriod"]:checked')?.value;
+
+//Line 5: reason
+function SaveReason(){
+    enterReason.disabled=true; //no more changing choice
+    saveReason.disabled=true; //no more saving, this is already saved
+    reasonOfLeave=enterReason.value; //set value 
+    console.log(reasonOfLeave) //debug testing
+    saveReason.style.backgroundColor= "grey";
+}
+saveReason.onclick=SaveReason;
+
+//cancel the input
+function CancelReason(){
+    enterReason.disabled=false; //no more changing choice
+    saveReason.disabled=false; //no more saving, this is already saved
+    reasonOfLeave=""; //wipe clean
+    console.log(reasonOfLeave) //debug testing
+    saveReason.style.backgroundColor= "green";
+}
+cancelReason.onclick=CancelReason;
+
+//LINE 6: SUBMISSION BOIIIIIIS we made it
+function Submission(){
+    //flexible values are now assigned to variable
+    dateOfApplication=enterDate.value;
+    timeNeeded= enterTime.value;
+    periodOfLeave = document.querySelector('input[name="enterPeriod"]:checked')?.value; //period number
+
+    //debug, testing allvariables are documented correctly. this will be the palce when they are posted
+    console.log(studentName+teacher+teacherEmail+timeNeeded+dateOfApplication+periodOfLeave+reasonOfLeave);
+
+    submission.style.backgroundColor= "grey";
+    submission.disabled=true;
+}
+
+submission.onclick= Submission;
+
+
+//HOME PAGE JS ENDS HERE
+
 
 const sheetInput = document.getElementById("testingInput");
 const sheetData = document.getElementById ("testingButton");
@@ -24,16 +166,8 @@ fetch("teacherNames.json")
             );
  });
 
-//line3 
-const enterTimeValue = document.getElementById("enterTimeValue");
-const enterTime = document.getElementById("enterTime");
 
-function enterTimeChange() {
-    var time=enterTime.value;
-    enterTimeValue.innerHTML = `MAX 60 min, MIN 1 min:<br> ${time} minutes`;
-}
 
-enterTime.addEventListener("input", enterTimeChange);
 
 
 
